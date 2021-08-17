bab1f1cb9489b66286e8480fcb421f96cfd7290a
launchpad5682 <launchpad5682@gmail.com>
2021-08-17 16:39:30 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+import SignUp from "./components/SignUp";
 import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./helper/PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="App"></div>
+      <Router>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/signout" component={SignOut} />
+          <PrivateRoute path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </Router>
     </AuthProvider>
   );
 }
