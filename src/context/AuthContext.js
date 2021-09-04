1fdaffd1e953fca162cf93caa68307e3e78961f6
launchpad5682 <launchpad5682@gmail.com>
2021-09-04 14:20:12 +0530

    get groups function implemented

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { auth } from "../auth/firebase";
+import { auth, fireDB, app } from "../auth/firebase";
 
 export const AuthContext = createContext();
 
@@ -9,6 +9,10 @@ export const AuthProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(null);
   // authentication will take time till then we can use loading to pass only when it is loaded
   const [loading, setLoading] = useState(true);
+
+  // user and group data for chats
+  const [groups, setGroups] = useState([]);
+  const [users, setUsers] = useState([]);
 
   //signup function
   function signup(email, password) {
@@ -26,6 +30,79 @@ export const AuthProvider = (props) => {
     return auth.signOut();
   }
 
+  async function getGroups() {
+    let tempGroups = [];
+    await fireDB
+      .collection("groups")
+      .get()
+      .then((snapshot) =>
+        snapshot.forEach((doc) => tempGroups.push(doc.data().group))
+      );
+
+    setGroups(tempGroups);
+  }
+
+  async function addGroup(groupName) {
+    
+  }
+
+  async function getUsers() {}
+
+  async function addUser2Group(user) {
+    const { uid, photoURL } = currentUser;
+    // fireDB
+    //   .collection("groups")
+    //   .doc("welcome")
+    //   .collection("group_data")
+    //   .doc("users")
+    //   .then((data) => console.log(data));
+    // // console.log(uid, photoURL);
+    // // return { uid, photoURL };
+    // await fireDB
+    //   .collectionGroup("groups")
+    //   .where("group", "==", "welcome")
+    //   .collection("group_data")
+    //   .get()
+    //   // .doc("welcome")
+    //   // .collection("group_data")
+    //   // .get()
+    //   .then((snapshot) => snapshot.forEach((doc) => console.log(doc.data())));
+
+    // await fireDB
+    //   .collection("groups")
+    //   .get()
+    //   .then((snapshot) =>
+    //     snapshot.forEach((data) => console.log(data.data()))
+    //   );
+
+    await fireDB
+      .collection("groups/welcome/group_data")
+      .get()
+      .then((snapshot) => snapshot.forEach((doc) => console.log(doc.data())));
+
+    // const arrayUnion = fireDB.FieldValue;
+    // console.dir(arrayUnion);
+
+    const doc = await fireDB.doc("groups/welcome/group_data/users");
+
+    doc.update({
+      items: fireDB.FieldValue.arrayUnion("Atharva"),
+    });
+
+    // await doc.update({
+    //   items: arrayUnion(user),
+    // });
+
+    // await fireDB
+    //   .collection("groups")
+    //   // .where("group", "==", "welcome")
+    //   .doc("welcome")
+    //   .collection("group_data")
+    //   .doc("users")
+    //   .get()
+    //   .then((snapshot) => console.table(snapshot));
+  }
+
   // useEffect hook to check the currently signed in user
   // https://firebase.google.com/docs/auth/web/manage-users
   useEffect(() => {
@@ -34,10 +111,7 @@ export const AuthProvider = (props) => {
       setLoading(false);
     });
 
-    return () => {
-      unsubscribe();
-      setCurrentUser(null);
-    };
+    return () => unsubscribe();
   }, []);
 
   const value = {
@@ -45,6 +119,9 @@ export const AuthProvider = (props) => {
     login,
     signup,
     signout,
+    addUser2Group,
+    getGroups,
+    groups,
   };
 
   return (
