099d82428da3b889a1dd8673a67e544eb32429a3
launchpad5682 <launchpad5682@gmail.com>
2021-09-13 22:09:22 +0530

    groups list rendered dynamically, search box added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "./Profile";
 import { HiPlus } from "react-icons/hi";
+import { useAuth } from "../context/AuthContext";
 
 function Main() {
+  const { groups, getGroups } = useAuth();
+
+  useEffect(() => {
+    getGroups();
+  }, []);
+
+  function addGroupHandler() {
+    console.log("add group clicked");
+  }
+
   return (
     <div className="flex text-white bg-black h-screen">
       <div className="w-80 p-3">
         <div className="flex justify-between align-middle">
           <h2 className="text-xl">Channels</h2>
-          <HiPlus className="text-2xl" />
+          <HiPlus className="text-2xl" onClick={addGroupHandler} />
         </div>
-        <div>Group description help desk fall apart to the end</div>
-        <h2>MEMEBERS</h2>
-        <div>
-          {/* {users
-            ? users.map((user) => (
-                <div className="flex-col">
-                  <Svg src={user.svg} alt="avatar" className="inline" />
-                  {user.name}
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full bg-gray-800 h-10"
+          ></input>
+        </form>
+        <div className="mt-5">
+          {groups
+            ? groups.map((group) => (
+                <div key={group} className="flex rounded-md">
+                  <div className="text-lg bg-gray-600 mr-2 rounded-md w-10 h-10 flex items-center justify-center">
+                    <text>{group[0].toUpperCase()}</text>
+                  </div>
+                  <div className="w-full flex items-center">
+                    {group[0].toUpperCase() + group.substring(1)}
+                  </div>
                 </div>
               ))
-            : null} */}
-          {/* Groups name */}
+            : null}
         </div>
         <Profile />
       </div>
