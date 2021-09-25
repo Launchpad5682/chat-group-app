f05384b3a1f845c4a8ec6be8b998ee95df0d65a5
launchpad5682 <launchpad5682@gmail.com>
2021-09-26 00:09:09 +0530

    Sign in redirection updated to groups page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "../styles/output.css";
 
 function SignIn() {
   const history = useHistory();
@@ -17,13 +18,13 @@ function SignIn() {
       setLoading(true);
       // waits until the login credentials are updated
       await login(email.current.value, password.current.value);
-      history.push("/dashboard");
+      history.push("/groups");
     } catch {}
   }
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <div className="bg-white p-12 h-auto w-96 space-y-8 rounded-lg shadow-2xl">
+    <div className="flex flex-col h-screen items-center justify-center bg-gray-800">
+      <div className="bg-gray-900 p-12 h-auto w-96 space-y-8 rounded-lg shadow-2xl text-white">
         {error && <div>Enter correct email and password</div>}
         <form onSubmit={submitHandler} className="space-y-5">
           <div>
@@ -35,7 +36,7 @@ function SignIn() {
                 ref={email}
                 placeholder="Enter your email"
                 required
-                className="w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-blue-500"
+                className="bg-transparent w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-green-500"
               ></input>
             </div>
           </div>
@@ -47,21 +48,24 @@ function SignIn() {
                 type="password"
                 ref={password}
                 placeholder="Enter your password"
-                className="w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-blue-500"
+                className="bg-transparent w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-green-500"
               ></input>
             </div>
           </div>
           <button
             disabled={loading}
             type="submit"
-            className="bg-blue-600 p-2 text-yellow-50 rounded-md min-w-full"
+            className="bg-green-600 p-2 text-yellow-50 rounded-md min-w-full"
           >
             Login
           </button>
         </form>
         <div className="text-left">
           Create an account?{" "}
-          <Link to="/signup" className="text-blue-900 underline px-2">
+          <Link
+            to="/signup"
+            className="text-green-900 text-lg font-bold underline px-2"
+          >
             Sign Up
           </Link>
         </div>
