b2f13e30c446f8de6650216453ef38c65eb2598f
launchpad5682 <launchpad5682@gmail.com>
2021-09-25 17:11:24 +0530

    Border updated of modal overlay

--- src/components/AddGroupDialog.js
+++ src/components/AddGroupDialog.js
@@ -22,27 +22,27 @@ function AddGroupDialog() {
   }
 
   return (
-    <div className="flex fixed inset-0 justify-center items-center bg-gray-900 bg-opacity-50 ">
+    <div className="flex fixed inset-0 justify-center items-center bg-gray-900 bg-opacity-50">
       <div className="bg-black w-4/12 h-96 px-6 py-4 rounded-xl text-white">
         <h1 className="text-white">NEW CHANNEL</h1>
         <form onSubmit={submitHandler}>
           <input
-            className="bg-gray-700 w-full outline-none my-4 py-2 px-3"
+            className="bg-gray-700 w-full outline-none my-4 py-2 px-3 rounded-md"
             placeholder="Channel name"
             ref={groupName}
           ></input>
           <textarea
-            className="bg-gray-700 w-full outline-none py-2 px-3 h-52 resize-none"
+            className="bg-gray-700 w-full outline-none py-2 px-3 h-52 resize-none rounded-md"
             placeholder="Channel description"
             ref={groupDesc}
           ></textarea>
           <button
-            className="bg-red-600 py-1 px-5 mt-2 float-right"
+            className="bg-red-600 py-1 px-5 mt-2 float-right rounded-md"
             onClick={closeHandler}
           >
             Close
           </button>
-          <button className="bg-green-600 py-1 px-5 mt-2 float-right mr-3">
+          <button className="bg-green-600 py-1 px-5 mt-2 float-right mr-3 rounded-md">
             Save
           </button>
         </form>
