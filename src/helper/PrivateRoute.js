810dfc5d4c2b7ceb041a08d5aa577affb62ef755
launchpad5682 <launchpad5682@gmail.com>
2021-09-27 21:51:00 +0530

    Private Route Updated, had syntax error. Curly braces're missing while deconstructing

--- src/helper/PrivateRoute.js
+++ src/helper/PrivateRoute.js
@@ -2,22 +2,22 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { Route, Redirect } from "react-router-dom";
 
-function PrivateRoute({ children, ...rest }) {
-  const currentUser = useAuth();
-
+function PrivateRoute({ component: Component, ...rest }) {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
   return (
     <Route
       {...rest}
-      render={({ location }) =>
-        currentUser !== null ? (
-          children
-        ) : (
+      render={(props) =>
+        currentUser === null ? (
           <Redirect
             to={{
               pathname: "/login",
-              state: { from: location },
+              state: { from: props.location },
             }}
           />
+        ) : (
+          <Component {...props} />
         )
       }
     />
