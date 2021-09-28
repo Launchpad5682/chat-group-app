bf3cc906b46d7b885960f9892c9a99b982fb5c7c
launchpad5682 <launchpad5682@gmail.com>
2021-09-28 19:39:49 +0530

    Drop Implemented without any functionality

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Svg from "react-inlinesvg";
 import { useAuth } from "../context/AuthContext";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { AiOutlineLogout } from "react-icons/ai";
 
 function Profile() {
   const { user, getAvatar } = useAuth();
+  //drop down
+  const [dropDownState, setDropDownState] = useState(false);
 
   const avatar = getAvatar(user);
+
+  //drop drop
+
+  const DropDown = () => {
+    return (
+      <div className="h-10 w-28 relative bg-gray-800 bottom-20 right-2 float-right">
+        Hello
+        <AiOutlineLogout />
+      </div>
+    );
+  };
+
   return (
-    <div className="bg-gray-900 h-12 flex items-center rounded-lg">
-      <div className="bg-gray-400 rounded-lg">
-        <Svg src={avatar} alt="avatar" className="inline" />
+    <>
+      <div className="bg-gray-900 h-12 flex items-center rounded-lg">
+        <div className="bg-gray-400 rounded-lg">
+          <Svg src={avatar} alt="avatar" className="inline" />
+        </div>
+        <div className="flex justify-between w-full text-xl px-2">
+          <span className="capitalize">{user}</span>
+          <span>
+            <MdKeyboardArrowDown
+              className="inline"
+              onClick={() => setDropDownState(!dropDownState)}
+            />
+          </span>
+        </div>
       </div>
-      <div className="flex justify-between w-full text-xl px-2">
-        <span className="capitalize">{user}</span>
-        <MdKeyboardArrowDown className="inline" />
-      </div>
-    </div>
+      {dropDownState && <DropDown />}
+    </>
   );
 }
 
