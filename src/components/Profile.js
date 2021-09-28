1cc2783b37e78298ed4767455a918d803fbf6f8e
launchpad5682 <launchpad5682@gmail.com>
2021-09-28 18:49:22 +0530

    Profile card added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Svg from "react-inlinesvg";
+import { useAuth } from "../context/AuthContext";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 function Profile() {
-  return <div></div>;
+  const { user, getAvatar } = useAuth();
+
+  const avatar = getAvatar(user);
+  return (
+    <div className="bg-gray-900 h-12 flex items-center rounded-lg">
+      <div className="bg-gray-400 rounded-lg">
+        <Svg src={avatar} alt="avatar" className="inline" />
+      </div>
+      <div className="flex justify-between w-full text-xl px-2">
+        <span className="capitalize">{user}</span>
+        <MdKeyboardArrowDown className="inline" />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
